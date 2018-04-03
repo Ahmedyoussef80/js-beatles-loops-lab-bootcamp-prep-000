514723dac8a3bbae21e6af865ed7c5e894473e71
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-03 23:39:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
-  var new_array = [];
-  var new_string;
+  var array = [];
+  var string;
   for (let i=0; i < musicians.length; i++) {
     new_string = musicians[i] + " plays " + instruments[i];
     new_array.push(new_string);
