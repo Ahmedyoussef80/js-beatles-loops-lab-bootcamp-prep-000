037464cad0d50b9e11456dc641b9379eee296392
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-03 23:39:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
- 
+function theBeatlesPlay(musicians, instruments) {
+  var new_array = [];
+  var new_string;
+  for (let i=0; i < musicians.length; i++) {
+    new_string = musicians[i] + " plays " + instruments[i];
+    new_array.push(new_string);
+  }
+  return new_array;
+}
