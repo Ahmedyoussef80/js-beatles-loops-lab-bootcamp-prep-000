a6b0dbd0eebb223d533652a0cf2137c64294318f
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-03 23:38:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+ var new_array = [];
+  var new_string;
+  for (let i=0; i < musicians.length; i++) {
+    new_string = musicians[i] + " plays " + instruments[i];
+    new_array.push(new_string);
+  }
+  return new_array;
+}
