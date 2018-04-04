582c799fa6551243d35673a4ba67327ae28537d3
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-04 00:23:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function johnLennonFacts(facts) {
   
   function iLoveTheBeatles(n){
   var beatles = [];
-  do {beatles.push('I love the Beatles!')*n
+  do {beatles.push('I love the Beatles!')
     n++;
   }
  while (n<15);
